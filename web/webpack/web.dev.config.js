9ff65d49f3a4bbc0bce948c287357c0b7e0975f0
Jonathan Kaufman <jondakauf@gmail.com>
2016-01-28 17:47:33 -0500

    initialize web directory

---
+++ web/webpack/web.dev.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    'babel-polyfill',
+    path.join(__dirname, '../../app/web/index'),
+  ],
+  output: {
+    path: path.join(__dirname, '../public'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    loaders: [
+      // take all less files, compile them, and bundle them in with our js bundle
+      { test: /\.less$/, loader: 'style!css!autoprefixer?browsers=last 2 version!less' },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react'],
+          plugins: [['react-transform', {
+            transforms: [{
+              transform: 'react-transform-hmr',
+              imports: ['react'],
+              // this is important for Webpack HMR:
+              locals: ['module']
+            }],
+          }]],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('development'),
+        PLATFORM_ENV: JSON.stringify('web'),
+      },
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+  ],
+};
